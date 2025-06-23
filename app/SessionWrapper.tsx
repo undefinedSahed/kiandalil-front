647a650fb6aecf870e7313fa120ae1b918415bbf
undefinedSahed <sahedrahmanshakil@gmail.com>
2025-06-23 15:36:40 +0600

    fix

--- app/SessionWrapper.tsx
+++ app/SessionWrapper.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 export default function SessionWrapper({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <Header />
+      {children}
+      <Footer />
+    </SessionProvider>
+  )
 }
