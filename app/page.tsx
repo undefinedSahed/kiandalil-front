3439b9f9ab0f3ed98ae74a004943c742c3edbc84
undefinedSahed <sahedrahmanshakil@gmail.com>
2025-11-16 01:40:31 +0600

    removed the stats section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function HomePage() {
     <div className="min-h-screen bg-white">
       <HeroSection />
       <CitiesSection />
-      <StatsSection />
+      {/* <StatsSection /> */}
       <FeaturedProperties />
       <AboutUsHome />
       <TestimonialsSection />
